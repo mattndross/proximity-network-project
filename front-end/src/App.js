c24602f2528929b0a11ea6333bee0621ba575290
mattndross <mattndross@users.noreply.github.com>
2021-12-01 16:30:28 +0100

    install boostrap

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <button className="btn btn-pn">Este es un boton de proximity network </button>
     </div>
   );
 }
