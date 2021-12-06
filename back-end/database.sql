3a66d4bc22383bf7a77cf45f70b5398882843f18
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2021-12-06 02:59:15 +0100

    nueva base datos

--- back-end/database.sql
+++ back-end/database.sql
@@ -1,34 +1,39 @@
-create table stores (
+gitcreate table stores (
     id              serial PRIMARY key,
     name            varchar(50) not null,
     mail            varchar(120) not null,
-    store_category  varchar(50),
+    store_category  varchar(50) ,
     web_page        varchar(100), 
-    image         
-    address         VARCHAR(120),
+    phone_number    varchar(20), 
+    image 			varchar(255)
+);
+
+create table store_location(
+	store_id		int ,
+	address         VARCHAR(120),
     city            VARCHAR(30),
     postcode        VARCHAR(12),
     country         VARCHAR(20), 
+    foreign key(store_id) references stores(id)
+ );
+
+ create table userStore_authentication(
+ 	store_id        int primary key, 
+ 	foreign key(store_id) references stores(id),
     store_manager   varchar(50) not null, 
-    manager_contact varchar(120) not null, 
-    password        varchar(20) not null
-)
+    mail 			varchar(120) not null, 
+    password 		varchar(100) not null
+    );
 
 create table products (
-    id              serial PRIMARY key,
-    store_id        int REFERENCES stores(id);
+    id              serial primary key,
+    store_id        int,
+    foreign key(store_id) REFERENCES stores(id),
     name            varchar(50) not null, 
     category        varchar(50) not null, 
     product_type    varchar(50),
     quantity_weight numeric(4,2), 
-    price           numeric(4,2),  
+    price           numeric(4,2), 
     producer        varchar(50), 
-    origin          varchar(50), 
-    image
-)
-
-insert into stores (name, mail, store_category, web_page, address, city, postcode, country, store_manager, manager_contact) 
-        values ('Ecoalimentaria', 'eco@mail.com', 'https://www.ecoalimentaria.es/es/', 'carrer canigo 94', 'Barcelona', '08031', 'Jose Luis Perez', 'pepe@mail.com');
-
-insert into products (name, category, product_type, quantity_weight, price, producer, origin)       
-        values ()
+    origin          varchar(50) 
+);
