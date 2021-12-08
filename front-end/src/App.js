8aaa7d4fd80a8b9d82e6b636352b66c1ac57533d
Giio12 <toiigmar@gmail.com>
2021-12-08 14:07:06 +0100

    Header terminado

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import "./App";
 import LandingPage from "./pages/LandingPage";
 import ProductList from "./pages/ProductsList";
+import Header from "./components/Header"
 
 function App() {
   return (
     <div className="wrapper">
+      <Header/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
---
+++ front-end/src/components/Header/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "../Header/img/logo.jpg"
+import icon from "../Header/img/icon.png"
+import {FaBars} from "react-icons/fa"
+import "../Header/Header.css"
+
+const Header = ()=>{
+    // <div className= "l-container"> 
+    return(
+        //*----Header---*//
+        <nav className= "main-header">
+            <a><img src={logo} alt="..." className="logo"></img></a>
+            <ul className="main-menu">
+                <li className="main-menu_item">
+                    <a className="main-menu_link" href="#">Become a Member</a></li>
+                <li className="main-menu_item">
+                    <a className="main-menu_link" href="#">Login<img src={icon} alt="..." className="icon"></img></a></li>         
+                <label for="check" className="checkbtn">
+                 <FaBars></FaBars>
+                </label>
+            </ul>
+        </nav>
+    )
+}
+export default Header;
