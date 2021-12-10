17021843f0126481e590e562554f8b2502dbe2b0
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2021-12-10 14:02:16 +0100

    products/:productId endpoint delete

--- back-end/server.js
+++ back-end/server.js
@@ -37,17 +37,6 @@ app.get('/stores/:city', (req, res)=>{ //return list of stores filtered by city
     .catch((e) => console.log(e));
 });
 
-app.get('/stores/search/:postCode',  (req,res) => { // return list of stores that have the given post code.
-    const postCode = req.params.postCode;
-    pool.query("SELECT name, store_description as Description, store_category as Category, web_page as Web, store_email as email, phone_number, image FROM stores as s join stores_locations as s_l on s_l.store_id = s.id WHERE postcode = $1", [postCode])
-    .then((result) => { if(result.rowCount == 0){
-                        res.send({message : "there are not stores in this area"})
-                    } else { 
-                        return res.json(result.rows)}
-                    })
-    .catch((error) => console.log(error));
-})
-
 app.get('/stores/search/:storeName', (req, res)=>{ //return list of stores if the given  filtered by name
     const storeName = `%${req.params.storeName}%`;
     pool.query("SELECT name, store_description as Description, store_category as Category, web_page as Web, store_email as email, phone_number, image FROM stores as s WHERE UPPER(name) LIKE UPPER($1) ORDER BY name", [storeName])
@@ -62,7 +51,6 @@ app.get('/stores/profile/:storeId', (req,res)=>{ //return data of given store
     .catch((e) => console.log(e));
 });
 
-//>>>>>> NO FUNCIONA DELETE METHOD <<<<<//
 app.delete('/products/:productId', (req, res) => {
     console.log(req)
     const idProduct = req.params.productId;
@@ -87,22 +75,6 @@ app.get('/products/storeProducts/:storeId', (req,res)=> { //return data of the g
     .then((result)=> res.json(result.rows))
     .catch((e)=> console.log(e));
 });
-
-app.post('/stores/profile/:idStore/addproducts/', (req, res) => { // the store add a product 
-    const idStore= req.params.idStore;
-    const { type, brand, category, description, unit, price, producer, origin } = req.body;
-    const query = 'INSERT INTO products (store_id, product_type, brand, category, product_description, unit, price, producer, origin) values ($1, $2, $3, $4, $5, $6, $7, $8, $9)';
-    
-    pool.query(query, [idStore, type, brand, category, description, unit, price, producer, origin,])
-    .then(() => res.status(200).send({message : "the product was added succesfully"}))
-    .catch(error => console.log(error))
-    //res.send(console.log(req.body))
-
-})
-         
-
-
-
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, ()=> console.log(`proximity network is running in port ${PORT}`));
