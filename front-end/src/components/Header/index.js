f49c827634921d9b56042b20c2858fc805491e30
annaLucian <gabrielapoves01@gmail.com>
2021-12-11 16:10:19 +0100

    add link router

--- front-end/src/components/Header/index.js
+++ front-end/src/components/Header/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import logo from "../Header/img/logo.png"
 import { FaBars } from "react-icons/fa"
+import { Link } from 'react-router-dom'
 import "../Header/Header.css"
 
 const Header = () => {
     // <div className= "l-container"> 
     return (
         <nav className="main-header px-5">
-            <a href="/"><img src={logo} className="img-fluid" width="150px" alt="..." className="logo"></img></a>
+            <Link to="/">
+                <img src={logo} className="img-fluid" width="150px" alt="..." className="logo" />
+            </Link>
+
             <ul className="main-menu">
                 <li className="main-menu_item">
                     <a className="main-menu_link" href="#">Become a Member</a></li>
