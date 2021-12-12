a4cd984b87363f523e3bf67869fc493158dd3c6a
annaLucian <gabrielapoves01@gmail.com>
2021-12-12 20:46:40 +0100

    improvements search bar

--- front-end/src/components/LandingPageSearchBar/index.js
+++ front-end/src/components/LandingPageSearchBar/index.js
@@ -1,7 +1,28 @@
+
+
 import "./LandingPageSearchBar.css";
 import iconSearch from "../../assets/img/search-landing/icon-search.png"
 import imagenSearch from "../../assets/img/search-landing/imagen-search.png"
+import { useState } from "react"
+
+
+
+
 const LandingPageSearchBar = () => {
+  const [postal, setPostal] = useState("");
+  const [city, setCity] = useState("ere");
+
+  const handleChangeCity = (event) => {
+    setCity(event.target.value)
+    console.log(city)
+  }
+
+  const searchStores = (event) => {
+    event.preventDefault()
+    console.log(city, postal)
+
+
+  }
   return (
     <section id="searchLanding" className="search-page">
       <div className="container">
@@ -16,18 +37,21 @@ const LandingPageSearchBar = () => {
               <img src={iconSearch} className="img-fluid" alt="icono del buscador" />
             </div>
             <h2>Find <span>the best organic stores in your city</span>, with quality products.</h2>
-            <form className="text-center">
+            <form className="text-center" onSubmit={searchStores}>
               <div className="row search-form">
-                <div className=" col-6 content-search-postal">
+                <div className=" col-6 content-select">
                   <di className="search-postal">
-                    <i class="bi bi-search"></i>
-                    <input type="number" placeholder="Search by zip code" />
+                    <select class="form-select" aria-label="Default select example">
+                      <option selected>Select search by</option>
+                      <option value="1">City</option>
+                      <option value="2">Postal code</option>
+                    </select>
                   </di>
                 </div>
-                <div className="col-6 content-search-city">
-                  <div className=" search-city">
-                    <i class="bi bi-geo-alt-fill"></i>
-                    <input type="text" placeholder="Search by city" />
+                <div className="col-6 content-search-city-postal">
+                  <div className=" search-city-postal">
+                    <i class="bi bi-search"></i>
+                    <input type="text" placeholder="city or zip code" value={city} onChange={handleChangeCity} />
                   </div>
                 </div>
               </div>
