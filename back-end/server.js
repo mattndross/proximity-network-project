8794f83836650253758e376561f89eaaa885019c
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2021-12-14 10:49:08 +0100

    trabajando en put/stores/:storeId

--- back-end/server.js
+++ back-end/server.js
@@ -7,7 +7,19 @@ app.use(express.json());
 const corsOptions = {
   origin: "http://localhost:3000",
 };
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)); 
+
+const { searchController } = require("./controllers/search.controller");
+
+const { Pool } = require('pg');
+const { reset } = require("nodemon");
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'proximity_network',
+    password: 'fran1985',
+    port: 5432
+});
 
 const { pool } = require("./pool");
 const authController = require("./controllers/auth.controller");
@@ -43,12 +55,27 @@ app.get("/products/:productId", publicController.findProductById);//devuelve los
 
 app.delete("/products/:productId", deleteProduct);
 
-app.get("/products/storeProducts/:storeId", (req, res) => {
-  //return data of the given product
-  const storeId = req.params.storeId;
+app.get('/stores/search/:storeName', (req, res)=>{ //return list of stores if the given  filtered by name
+    const storeName = `%${req.params.storeName}%`;
+    pool.query("SELECT name, store_description as Description, store_category as Category, web_page as Web, store_email as email, phone_number, image FROM stores as s WHERE UPPER(name) LIKE UPPER($1) ORDER BY name", [storeName])
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.log(e));
+});
 
-  pool
-    .query("SELECT * FROM products WHERE store_id = $1", [storeId])
+app.get('/stores/postcode/:postCode',  (req,res) => { // return list of stores that have the given post code.
+    const postCode = req.params.postCode;
+    pool.query("SELECT name, store_description as Description, store_category as Category, web_page as Web, store_email as email, phone_number, image FROM stores as s join stores_locations as s_l on s_l.store_id = s.store_id WHERE postcode = $1", [postCode])
+    .then((result) => { if(result.rowCount == 0){
+                        console.log(result);
+                        res.send({message : "there are not stores in this area"})
+                        } else { 
+                        return res.json(result.rows)}
+                    })
+    .catch((error) => console.log(error));
+})
+app.get('/stores/profile/:storeId', (req,res)=>{ //return data of given store
+    const storeId = req.params.storeId; 
+    pool.query('SELECT * FROM stores WHERE store_id = $1', [storeId])
     .then((result) => res.json(result.rows))
     .catch((e) => console.log(e));
 });
@@ -81,34 +108,32 @@ app.post("/stores/profile/:idStore/addproducts", (req, res) => {
   //res.send(console.log(req.body))
 });
 //PUT/products/:productId |-- la tienda edita los datos de un producto en particular
-app.put("/products/:productId", (req, res) => {
-  const idProduct = req.params.productId;
-  const { type, brand, category, description, unit, price, producer, origin } =
-    req.body;
-  const query =
-    "UPDATE products SET product_type = $1, brand = $2, category = $3, product_description = $4, unit = $5, price = $6, producer = $7, origin = $8 WHERE id = $9";
-  pool
-    .query(query, [
-      type,
-      brand,
-      category,
-      description,
-      unit,
-      price,
-      producer,
-      origin,
-      idProduct,
-    ])
-    .then((result) => {
-      if (result.rowCount === 0) {
-        return res.send({ message: `id number ${idProduct} does not exist` });
-      } else {
-        // console.log(result);
-        return res.send({ message: "your change was submitted" });
-      }
-    })
-    .catch((e) => console.log(e));
-});
+app.put('/products/:productId', (req, res) =>{
+    const idProduct = req.params.productId;
+    const { type, brand, category, description, unit, price, producer, origin } = req.body;
+    const query = 'UPDATE products SET product_type = $1, brand = $2, category = $3, product_description = $4, unit = $5, price = $6, producer = $7, origin = $8 WHERE id = $9'
+   pool.query(query, [type, brand, category, description, unit, price, producer, origin, idProduct])
+        .then((result) => {if (result.rowCount === 0) { 
+                        return res.send({message: `id number ${idProduct} does not exist`})
+                      } 
+                        else  {
+                        // console.log(result);
+                       return res.send({message : "your change was submitted"})
+                    }})
+        .catch((e)=> console.log(e))
+
+})
+//PUT/stores/:storeId?section |-- la tienda puede editar una sección(columna) específica de su perfil.
+app.put('/stores/profile/:storeId', (req, res)=>{ // aqui solo cambia de las tablas stores y stores_locations
+    const idStore = req.params.storeId;
+    const {address, city, postcode, country} = req.body;
+    const query = 'UPDATE stores_locations as s_l SET  address = $1, city = $2, postcode = $3, country = $4 where store_id = $5';
+    pool.query(query, [address, city, postcode, country, idStore])
+        .then((result)=>{console.log(result.rows); res.send(result)})
+    
+})
+
+ 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () =>
   console.log(`proximity network is running in port ${PORT}`)
