ab4abbfc08e8e037f7cf44a6de68936f2893b41a
Giio12 <toiigmar@gmail.com>
2021-12-15 23:36:55 +0100

    Header responsive menu desplegable

--- front-end/src/components/Header/index.js
+++ front-end/src/components/Header/index.js
@@ -1,25 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "../Header/img/logo.png"
-import { FaBars } from "react-icons/fa"
+import { FaBars, FaTimes} from "react-icons/fa"
 import { Link } from 'react-router-dom'
 import "../Header/Header.css"
 
 const Header = () => {
     // <div className= "l-container"> 
-    return (
-        <nav className="main-header px-5">
-            <Link to="/">
-                <img src={logo} className="img-fluid" width="150px" alt="..." className="logo" />
-            </Link>
+    const [open, setOpen] = useState(false)
 
-            <ul className="main-menu">
+    const handleChange = () => {
+        setOpen(!open)
+    }
+    
+    return (
+        <nav className={`main-header px-5 ${open ? 'open' : ''}`}>
+            <Link to="/">
+            <img src={logo} alt="..." className="logo"></img>
+            </Link>
+            <input type="checkbox" id="check" onChange={handleChange}></input>
+            <label for="check" className="checkbtn">
+            <i class="bi bi-list"></i>
+            </label>
+            <ul className="main-menu">            
                 <li className="main-menu_item">
                     <a className="main-menu_link" href="#">Become a Member</a></li>
                 <li className="main-menu_item">
                     <a className="main-menu_link" href="#">Login <i class="bi bi-person-circle icon-navbar"></i></a></li>
-                <label for="check" className="checkbtn">
-                    <FaBars></FaBars>
-                </label>
             </ul>
         </nav >
     )
