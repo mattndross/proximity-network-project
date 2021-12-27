5423fdb0ede397986d34f1228136e0cba41c7ed4
mattndross <mattndross@users.noreply.github.com>
2021-12-27 12:40:16 +0100

    agrega funcion de logout

--- front-end/src/services/authentication.service.js
+++ front-end/src/services/authentication.service.js
@@ -2,4 +2,8 @@ exports.getCurrentUser = () => {
     const currentUser = JSON.parse(localStorage.getItem("user"));
     console.log({currentUser});
     return currentUser;
+};
+
+exports.logout = () => {
+    return localStorage.removeItem("user");
 }
