6af58a79e42342efb6b3c3407d6848d0c945e5f1
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2021-12-27 20:40:35 +0100

    hook form

--- front-end/src/components/ModalRegister/index.js
+++ front-end/src/components/ModalRegister/index.js
@@ -1,44 +1,84 @@
+import { useState, useRef } from 'react';
+import { useNavigate } from 'react-router';
+import { useForm } from 'react-hook-form';
 import './ModalRegister.css'
-import { Link } from 'react-router-dom'
-import React,{ useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import { Modal } from 'bootstrap';
+
+
+const validationSchema = Yup.object().shape({
+    storeManager: Yup.string().required('this field is required'),
+    managerEmail: Yup.string().email().required('this field is required'),
+    password: Yup.string().required('this field is required'),
+    confirmPassword: Yup.string().oneOf([Yup.ref("password"), null])
+});
 
 const Modalregister = () => {
-let navigate  = useNavigate();
+    let myModalRef = useRef();
+    let navigate = useNavigate();
+    const [storeManager, setStoreManager] = useState("")
+    const [managerEmail, setManagerEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-const [storeManager, setStoreManager] = useState("")
-const [managerEmail, setManagerEmail] = useState("")
-const [password, setPassword] = useState("")
+    //  al registrarse se redirigira a esta pagina--> /profile-user
+    const fetchRegister = async () => {
+        const dismissModal = () => {
+            console.log('modal tiene que cerrarse')
 
-//  al registrarse se redirigira a esta pagina--> /profile-user
-const fetchRegister =  async () => {
-        const url = "http://localhost:4000/register"; 
-        const body = { storeManager: storeManager, managerEmail: managerEmail, password: password};  
+            modal.hide(myModal);
+            myModal.addEventListener('hidden.bs.modal', () => {
+                document.body.style.overflow = 'unset';
+                document.body.style.paddingRight = '0';
+                if (document.querySelector(".modal-backdrop")) document.querySelector('.modal-backdrop').remove()
+            })
+        }
+
+        const url = "http://localhost:4000/register";
+        const body = { storeManager: storeManager, managerEmail: managerEmail, password: password };
         const config = {
             method: "POST",
-            headers: { 'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(body)
+        };
+
+        let myModal = myModalRef.current;
+        let modal = Modal.getInstance(myModal);
+
+        try {
+            const response = await fetch(url, config)
+            console.log("response", response);
+            if (!response.ok) {
+                if (response.status === 401) {
+                    modal.show(myModal);
+                }
+                console.log("response.status", response.status);
+
+            } else {
+                const data = await response.json()
+                console.log("data", data);
+                localStorage.setItem("token", data.token);
+                dismissModal();
+                navigate("/profile-user", { "replace": true })
+
+            }
+        } catch (e) {
+            console.log("error", e);
         }
-        try{
-        const response = await fetch(url, config)
-        console.log("response", response);
-        const data = await response.json()
-            console.log("data", data);
-            localStorage.setItem("token", data.token)
-            navigate("/profile-user", {replace: false})
-           
-    } catch(e) {
-        console.log("error", e);
-    }
-}
+        console.log("localStorage token", localStorage.getItem("token"));
 
-const registerDone = (e) => {
-    e.preventDefault();
-    fetchRegister()
+    };
 
-}
+
+    const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(validationSchema) });
+
+    const onSubmit = data => {
+        console.log(JSON.stringify(data, null, 2));
+
+        fetchRegister();
+    };
     return (
-        <div className="modal fade" id="modalRegister" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal fade" id="modalRegister" ref={myModalRef} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div className="modal-dialog modal-dialog-centered ">
                 <div className="modal-content">
                     <div className="modal-header">
@@ -53,45 +93,65 @@ const registerDone = (e) => {
                                 <i className="bi bi-person-bounding-box"></i>
                                 <h5>Store manager:</h5>
                             </div>
-                            <form className="form-register" onSubmit={registerDone} >
-                                <div className="mb-3">
+                            <form className="form-register" onSubmit={handleSubmit(onSubmit)} >
+                                <div className="mb-3 modal-body-content">
                                     <label htmlFor="exampleInputName1" className="form-label">Fullname<span>* </span></label>
-                                    <input 
-                                        type="text" 
-                                        className="form-control input-register" 
-                                        aria-label="FullName" 
-                                        value={storeManager} 
-                                        onChange={(e)=>{setStoreManager(e.target.value)}} 
-                                        required
+                                    <input
+                                        type="text"
+                                        ref={register}
+                                        className={`form-control input-register  ${errors.storeManager ? 'is-invalid' : ''}`}
+                                        aria-label="storeManager"
+                                        {...register('storeManager')}
+                                        value={storeManager}
+                                        onChange={(e) => { setStoreManager(e.target.value) }}
                                     />
-
                                 </div>
-                                <div className="mb-3">
+                                <p>{errors.storeManager?.message}</p>
+                                <div className="mb-3 modal-body-content">
                                     <label htmlFor="exampleInputEmail1" className="form-label">Email address<span>* </span></label>
-                                    <input 
-                                        type="email" 
-                                        className="form-control input-register" 
-                                        id="inputEmail" aria-describedby="emailHelp" 
-                                        value={managerEmail} 
-                                        onChange={(e)=>{setManagerEmail(e.target.value)}} 
-                                        required 
+                                    <input
+                                        type="email"
+                                        ref={register}
+                                        className={`form-control input-register  ${errors.managerEmail ? 'is-invalid' : ''}`}
+                                        id="inputEmail" aria-label="managerEmail"
+                                        {...register('managerEmail')}
+                                        value={managerEmail}
+                                        onChange={(e) => { setManagerEmail(e.target.value) }}
                                     />
                                 </div>
-                                <div className="mb-3">
+                                <p>{errors.managerEmail?.message}</p>
+
+                                <div className="mb-3 modal-body-content">
                                     <label htmlFor="exampleInputPassword1" className="form-label">Password<span>* </span></label>
-                                    <input 
-                                        type="password" 
-                                        className="form-control input-register" 
-                                        id="exampleInputPassword1" 
-                                        value={password} 
-                                        onChange={(e)=>{setPassword(e.target.value)}} 
-                                        required
+                                    <input
+                                        type="password"
+                                        ref={register}
+                                        className={`form-control input-register  ${errors.password ? 'is-invalid' : ''}`}
+                                        id="exampleInputPassword1" aria-label="password"
+                                        {...register('password')}
+                                        value={password}
+                                        onChange={(e) => { setPassword(e.target.value) }}
                                     />
                                 </div>
+                                <p>{errors.password?.message}</p>
+
+                                <div className="mb-3 modal-body-content">
+                                    <label htmlFor="exampleInputPassword1" className="form-label">Confirm Password<span>* </span></label>
+                                    <input
+                                        type="password"
+                                        ref={register}
+                                        className={`form-control input-register  ${errors.confirmPassword ? 'is-invalid' : ''}`}
+                                        id="exampleInputPassword1" aria-label="confirmPassword"
+                                        {...register('confirmPassword')}
+
+                                    />
+                                </div>
+                                <p>{errors.confirmPassword && "passwords should match"}</p>
+
                                 <button type="submit" className="btn btn-primary btn-register" >Create an account</button>
                             </form>
-                            <div className='text-center mt-2 mb-5'>
-                                <a  className="enlace-login" data-bs-toggle="modal" data-bs-target="#loginModal">Login </a>
+                            <div className='modal-login-link d-flex'>
+                                <a className="enlace-login" data-bs-toggle="modal" data-bs-target="#loginModal">Login </a>
                             </div>
                         </div>
                     </div>
