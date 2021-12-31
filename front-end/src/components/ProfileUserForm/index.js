e99d01a616ef77a23fc56d2ad302f6e23ac5d2cc
annaLucian <gabrielapoves01@gmail.com>
2021-12-31 12:46:42 +0100

    profileuser formulario

--- front-end/src/components/ProfileUserForm/index.js
+++ front-end/src/components/ProfileUserForm/index.js
@@ -18,7 +18,7 @@ const ProfileUserForm = ({ profile, setAction }) => {
         storeName: Yup.string().required('Store name is required'),
         storeDescription: Yup.string().required('Description is required'),
         storeStreet: Yup.string().required('Street is required').min(2, 'Seems a bit short EX: Carrer Felipe .....'),
-        postalCode: Yup.string().required('Postal code is required'),
+        postcode: Yup.string().required('Postal code is required'),
         city: Yup.string().required('City is required'),
         country: Yup.string().required('Country is required'),
         storeCategory: Yup.string().required('Store category is required'),
@@ -35,53 +35,34 @@ const ProfileUserForm = ({ profile, setAction }) => {
     // Metodo onSubmit
     const onSubmit = data => {
 
-        // console.log(JSON.stringify(data, null, 2));
-
-        const obj = {};
-        obj.storeName = data.storeName
-        obj.storeDescription = data.storeDescription
-        obj.storeDescription = data.storeDescription
-        obj.storeCategory = data.storeCategory
-        obj.storeWeb = data.storeWeb
-        obj.phoneNumber = data.phoneNumber
-        obj.imageUrl = "wwww.sadasd.com"
-        obj.storeStreet = data.storeStreet
-        obj.city = data.city
-        obj.country = data.country
-        obj.postcode = data.postalCode
+        console.log(data);
 
 
-        try {
+
+        const toasId = toast.custom(<div>
+            <button class="btn btn-primary button-loading-user" style={{ backgroundColor: "#408e0a", opacity: "1", fontWeight: "700" }} type="button" disabled>
+                <span class="spinner-border spinner-border-sm" style={{ color: "white", fontSize: "26px" }} role="status" aria-hidden="true"></span>
+                Loading...
+            </button>
+        </div>
+
+        );
 
 
-            const toastId = toast.loading('Loading..', {
-                style: {
-                    backgroundColor: "hsl(140,40%,55%)",
 
-                    color: "white",
-                    padding: "20px",
-                    width: "290px",
-
-                },
-                iconTheme: {
-                    primary: '#000',
-                    secondary: '#fff',
-
-                },
-            });
-            setTimeout(() => {
-                ProfileUserService.updateProfile(obj).then(
+        setTimeout(() => {
+            try {
+                ProfileUserService.updateProfile(data).then(
                     (response) => {
                         console.log(response);
-
-                        toast.dismiss(toastId);
+                        toast.remove(toasId)
 
                         toast.success("Profile updated successfully!", {
-                            duration: 1000,
                             style: {
-                                backgroundColor: "hsl(140,40%,55%)",
+                                backgroundColor: "#408e0a",
                                 color: "white",
-                                padding: "20px"
+                                padding: "20px",
+                                fontSize: "20px"
                             },
                         })
 
@@ -89,14 +70,20 @@ const ProfileUserForm = ({ profile, setAction }) => {
                         setAction("Actualizado")
                     }
                 );
-            }, 1000)
+            } catch (error) {
+                toast.error("error")
+            }
 
-        } catch (error) {
-            console.log("ERROR FORM PROFILE => ", error)
-        }
+        }, 1000)
+
+
 
     };
+    const onChangeSwitchButton = (e) => {
 
+        setChecked(!checked)
+
+    }
     // FormProfi
     const formProfile = (<div className='from-profile'>
         <div class="form-check form-switch">
@@ -108,15 +95,15 @@ const ProfileUserForm = ({ profile, setAction }) => {
             <div className="row mb-3">
                 <label htmlFor="exampleInputName1" className="form-label col-lg-4 col-form-label">Full name<span>* </span></label>
                 <div className="col-lg-8">
-                    <input type="text" defaultValue={profileInfo["store_manager"]} name="fullname" className={`form-control  ${errors.fullname ? 'is-invalid' : ''}`} id="exampleInputName" {...register('fullname')}
-                        disabled={!checked} />
+                    <input type="text" defaultValue={profileInfo["store_manager"]} name="fullname" className={`form-control  ${errors.fullname ? '' : ''}`} id="exampleInputName" {...register('fullname')}
+                    />
                     <div className="invalid-feedback">{errors.fullname?.message}</div>
                 </div>
             </div>
             <div className="row mb-3">
                 <label htmlFor="exampleInputStoreCategory" className="form-label col-lg-4 col-form-label">Store category<span>* </span></label>
                 <div className='col-lg-8'>
-                    <input type="text" name="storeCategory" defaultValue={profileInfo["store_category"]} className={`form-control  ${errors.storeCategory ? 'is-invalid' : ''}`} {...register('storeCategory')} id="exampleInputStoreName1" disabled={!checked} />
+                    <input type="text" name="" defaultValue={profileInfo["store_category"]} className={`form-control  ${errors.storeCategory ? 'is-invalid' : ''}`} {...register('storeCategory')} id="exampleInputStoreName1" />
                     <div className="invalid-feedback">{errors.storeCategory?.message}</div>
                 </div>
             </div>
@@ -132,7 +119,7 @@ const ProfileUserForm = ({ profile, setAction }) => {
             <div className="row mb-3">
                 <label htmlFor="exampleInputStoreName" className="form-label col-lg-4 col-form-label">Store name<span>* </span></label>
                 <div className='col-lg-8'>
-                    <input type="text" name="storeName" defaultValue={profileInfo.name} className={`form-control ${errors.storeName ? 'is-invalid' : ''}`} {...register('storeName')} id="exampleInputStoreName1" disabled={!checked} />
+                    <input type="text" name="storeName" defaultValue={profileInfo.name} className={`form-control ${errors.storeName ? 'is-invalid' : ''}`} {...register('storeName')} id="exampleInputStoreName1" />
                     <div className="invalid-feedback">{errors.storeName?.message}</div>
                 </div>
 
@@ -140,32 +127,32 @@ const ProfileUserForm = ({ profile, setAction }) => {
             <div className=" row mb-3">
                 <label htmlFor="exampleInputPDescription" className="form-label col-lg-4 col-form-label">Description<span>* </span></label>
                 <div className="form-floating col-lg-8">
-                    <textarea name="storeDescription" defaultValue={profileInfo["store_description"]} className={`form-control text-from-description ${errors.storeDescription ? 'is-invalid' : ''}`} {...register('storeDescription')} id="floatingTextarea2" style={{ height: "120px", width: "100%" }} disabled={!checked}></textarea>
+                    <textarea name="storeDescription" defaultValue={profileInfo["store_description"]} className={`form-control text-from-description ${errors.storeDescription ? 'is-invalid' : ''}`} {...register('storeDescription')} id="floatingTextarea2" style={{ height: "120px", width: "100%" }} ></textarea>
                     <div className="invalid-feedback">{errors.storeDescription?.message}</div>
                 </div>
             </div>
             <div className="row mb-3">
                 <label htmlFor="inputAddress2" className="form-label col-lg-4 col-form-label mb-2 pt-lg-0">Store address<span>* </span></label>
-                <div className="col-lg-8">
+                <div className="col-lg-8 mt-lg-4">
                     <div className='row'>
-                        <div className="col-6 mb-3" style={{ paddingRight: "6px" }}>
-                            <label htmlFor="inputAddress2" className="form-label col-lg-4 col-form-label mb-2 pt-lg-0">Street</label>
-                            <input type="text" name="storeStreet" defaultValue={profileInfo.address} className={`form-control title-placeholder ${errors.storeStreet ? 'is-invalid' : ''}`} {...register('storeStreet')} id="exampleInputStreet1" aria-describedby="emailHelp" disabled={!checked} />
+                        <div className="col-6 mb-3 formulario-address" style={{ paddingRight: "6px" }}>
+                            <label htmlFor="inputAddress2" className="form-label col-form-label mb-2 pt-lg-0">Street</label>
+                            <input type="text" name="storeStreet" defaultValue={profileInfo.address} className={`form-control title-placeholder ${errors.storeStreet ? 'is-invalid' : ''}`} {...register('storeStreet')} id="exampleInputStreet1" aria-describedby="emailHelp" />
                             <div className="invalid-feedback">{errors.storeStreet?.message}</div>
                         </div>
-                        <div className="col-6 mb-3" style={{ paddingLeft: "0px" }}>
-                            <label htmlFor="inputAddress2" className="form-label col-lg-4 col-form-label mb-2 pt-lg-0">Postal code</label>
-                            <input type="text" name="postalCode" defaultValue={profileInfo.postcode} className={`form-control title-placeholder ${errors.postalCode ? 'is-invalid' : ''}`} {...register('postalCode')} id="exampleInputCodePostal1" aria-describedby="emailHelp" disabled={!checked} />
-                            <div className="invalid-feedback">{errors.postalCode?.message}</div>
+                        <div className="col-6 mb-3 formulario-address" style={{ paddingLeft: "0px" }}>
+                            <label htmlFor="inputAddress2" className="form-label col-form-label mb-2 pt-lg-0">Postal code</label>
+                            <input type="text" name="postcode" defaultValue={profileInfo.postcode} className={`form-control title-placeholder ${errors.postcode ? 'is-invalid' : ''}`} {...register('postcode')} id="exampleInputCodePostal1" aria-describedby="emailHelp" />
+                            <div className="invalid-feedback">{errors.postcode?.message}</div>
                         </div>
-                        <div className="col-6 " style={{ paddingRight: "6px" }}>
-                            <label htmlFor="inputAddress2" className="form-label col-lg-4 col-form-label mb-2 pt-lg-0">City</label>
-                            <input type="text" name="city" defaultValue={profileInfo.city} className={`form-control title-placeholder ${errors.city ? 'is-invalid' : ''}`} {...register('city')} id="inputCity" aria-describedby="emailHelp" disabled={!checked} />
+                        <div className="col-6 formulario-address" style={{ paddingRight: "6px" }}>
+                            <label htmlFor="inputAddress2" className="form-label  col-form-label mb-2 pt-lg-0">City</label>
+                            <input type="text" name="city" defaultValue={profileInfo.city} className={`form-control title-placeholder ${errors.city ? 'is-invalid' : ''}`} {...register('city')} id="inputCity" aria-describedby="emailHelp" />
                             <div className="invalid-feedback">{errors.city?.message}</div>
                         </div>
-                        <div className="col-6" style={{ paddingLeft: "0px" }}>
-                            <label htmlFor="inputAddress2" className="form-label col-lg-4 col-form-label mb-2 pt-lg-0">Country</label>
-                            <input type="text" name="country" defaultValue={profileInfo.country} className={`form-control title-placeholder ${errors.country ? 'is-invalid' : ''}`} {...register('country')} id="exampleInputCountry1" aria-describedby="emailHelp" disabled={!checked} />
+                        <div className="col-6 formulario-address" style={{ paddingLeft: "0px" }}>
+                            <label htmlFor="inputAddress2" className="form-label  col-form-label mb-2 pt-lg-0">Country</label>
+                            <input type="text" name="country" defaultValue={profileInfo.country} className={`form-control title-placeholder ${errors.country ? 'is-invalid' : ''}`} {...register('country')} id="exampleInputCountry1" aria-describedby="emailHelp" />
                             <div className="invalid-feedback">{errors.country?.message}</div>
                         </div>
                     </div>
@@ -174,20 +161,20 @@ const ProfileUserForm = ({ profile, setAction }) => {
             <div className="row mb-3">
                 <label htmlFor="exampleInputStoreWeb1" className="form-label col-lg-4 col-form-label">Store web<span>* </span></label>
                 <div className='col-lg-8'>
-                    <input type="text" name="storeWeb" defaultValue={profileInfo["web_page"]} className={`form-control  ${errors.storeWeb ? 'is-invalid' : ''}`} {...register('storeWeb')} id="exampleInputPassword1" disabled={!checked} />
+                    <input type="text" name="storeWeb" defaultValue={profileInfo["web_page"]} className={`form-control  ${errors.storeWeb ? 'is-invalid' : ''}`} {...register('storeWeb')} id="exampleInputPassword1" />
                     <div className="invalid-feedback">{errors.storeWeb?.message}</div>
                 </div>
             </div>
             <div className="row mb-3">
                 <label htmlFor="exampleNumber1" className="form-label col-lg-4 col-form-label">Phone Number<span>* </span></label>
                 <div className='col-lg-8'>
-                    <input type="text" name="phoneNumber" defaultValue={profileInfo["phone_number"]} className={`form-control  ${errors.phoneNumber ? 'is-invalid' : ''}`} {...register('phoneNumber')} id="exampleInputPassword1" disabled={!checked} />
+                    <input type="text" name="phoneNumber" defaultValue={profileInfo["phone_number"]} className={`form-control  ${errors.phoneNumber ? 'is-invalid' : ''}`} {...register('phoneNumber')} id="exampleInputPassword1" />
                     <div className="invalid-feedback">{errors.phoneNumber?.message}</div>
                 </div>
             </div>
 
             <div className="d-flex flex-column flex-lg-row align-items-center justify-content-center gap-lg-4">
-                <button type="submit" className="btn btn-primary btn-formulario" disabled={!checked}>
+                <button type="submit" className="btn btn-primary btn-formulario" >
 
                     Save
                 </button>
