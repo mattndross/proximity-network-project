56440a2ca81ac46dfb3445b68e0f9a28aea718fd
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2022-01-03 04:18:34 +0100

    agregando pop up al clickear en marcadores de tiendas

--- front-end/src/components/Map/Markers.js
+++ front-end/src/components/Map/Markers.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Marker } from 'react-leaflet';
+import { useState } from 'react';
+import { Marker, Popup } from 'react-leaflet';
 import "leaflet/dist/leaflet.css"
 import IconLoc from './IconLoc';
 import * as data from "../Map/MapView/tiendasCoordenadas.json"
@@ -10,23 +11,27 @@ const Markers = () => {
 
     // const position = [41.391111, 2.152504]
     // <Marker position={position} icon={IconLoc}/>
-
     return (
-        
- <>
-     {data.stores.map(store => 
-        <Marker 
-        key={store.id}
-        position={[store.coordinates[0], store.coordinates[1]]}
-        icon={IconLoc} 
-        />
 
-)
-}
-</>
-      
+        <div>
+            {data.stores.map(store =>
+                <Marker
+                    key={store.id}
+                    position={[store.coordinates[0], store.coordinates[1]]}
+                    icon={IconLoc}
+                >)
+                    <Popup>
+                        <div><h6 style={{color:"#80b13d"}}>{store.name}</h6></div>
+                    </Popup>
+                </Marker>)
+
+            }
+
+        </div>
+
 
     )
 }
 
 export default Markers;
+
